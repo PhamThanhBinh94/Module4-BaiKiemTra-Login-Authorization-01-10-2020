074ae62ef4f9cd1a5a1ac28ef874125dae6db0a2
thanhbinh2612 <thanhbinh261294@gmail.com>
2020-10-01 17:09:26 +0700

    Login-Authorization-Bài kiểm tra ngày 01-10-2020

---
+++ src/main/java/com/codegym/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.codegym;
+
+import com.codegym.service.IMyUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private IMyUserService myUserService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService((UserDetailsService) myUserService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasRole("USER")
+                .and().formLogin()
+                .and().csrf()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    }
+
+}
---
+++ src/main/java/com/codegym/service/IMyUserService.java
@@ -0,0 +1,11 @@
+package com.codegym.service;
+
+import com.codegym.models.MyUser;
+
+import java.util.List;
+
+public interface IMyUserService {
+
+    List<MyUser> getUserList();
+
+}
